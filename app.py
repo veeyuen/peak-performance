49f72e01e3ba11bfbc118e3e0415a9a4b9488960
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-03-23 15:18:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -143,7 +143,7 @@ fig.update_layout(
     template="plotly_dark",
 
     legend1=dict( 
-        title= "By country",
+        title= "RECOVERY",
         x=1, 
         y=0, 
         title_font_family="Times New Roman", 
