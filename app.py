8c2bf1bbaa1ba4a9549b7c3b912dc7f6da74f4f1
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-03-25 05:25:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ fig.update_polars(
 )
 
 fig.update_xaxes(title_text="Days", row=2, col=1)
-fig.update_yaxes(title_text="Stress<br>Activity", row=2, col=1)
+fig.update_yaxes(title_text="Recovery<br>Activity", row=2, col=1)
 fig.update_xaxes(title_text="Days", row=2, col=2)
 fig.update_yaxes(title_text="Nutrition<br>Activity", row=2, col=2)
 
