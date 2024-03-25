be09ee6273e96066e7d4aaeec4a3becb67fa72bd
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-03-25 09:03:47 +0800

    Update app.py

--- app.py
+++ app.py
@@ -119,7 +119,7 @@ fig.add_trace(
 
 fig.add_trace(
     go.Scatterpolar(
-        theta=["<b>Activity<br>Level<b>", "<b>Strength<br>Levek<b>", "<b>Stamina<b>"],
+        theta=["<b>Activity<br>Level<b>", "<b>Strength<br>Level<b>", "<b>Stamina<b>"],
         r=[2,5,8],
         fill='toself',
         name='Day 0',
@@ -130,7 +130,7 @@ fig.add_trace(
 )
 fig.add_trace(
     go.Scatterpolar(
-        theta=["<b>Activity<br>Level<b>", "<b>Strength<br>Levek<b>", "<b>Stamina<b>"],
+        theta=["<b>Activity<br>Level<b>", "<b>Strength<br>Level<b>", "<b>Stamina<b>"],
         r=[7,3,9],
         fill='toself',
         name='Day 15',
