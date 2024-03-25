f33f9a035666fb8966be7d087bb696dacceb2680
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-03-25 09:13:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ fig.update_layout(
 #    minreducedwidth=350,
 #    minreducedheight=350,
     width=850,
-    height=850,
+    height=1200,
 #    title_text = 'PEAK PERFORMANCE',
 
     font=dict(
