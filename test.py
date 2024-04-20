05232016437172aa8f2af1fa6c5fff21de980444
Veesheen Yuen <116419752+veeyuen@users.noreply.github.com>
2024-04-20 15:08:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -165,7 +165,7 @@ fig.update_layout(
 
     font=dict(
         family="Courier New, monospace",
-        size=8,  # Set the font size here
+        size=10,  # Set the font size here
         color="White"
     ),
 
